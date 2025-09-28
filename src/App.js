9bdc085685cf87a4ce7b32c07c8ad57d5802288c
Devang <devangranderia9@gmail.com>
2025-09-28 21:17:56 +0530

    speed insight added

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import Main from './components/Main';
 import Skills from './components/Skills';
 import Divider from './components/Divider';
 import Overview from './components/Overview';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 function App() {
   return (
     <>
+      <SpeedInsights/>
       <Main/>
       <Divider/>
       <Overview/>
