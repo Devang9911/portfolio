1bf1c424299415ec772fd3cbcaad1002255ae564
Devang_Randeria <devangranderia9@gmail.com>
2025-12-05 10:01:00 +0000

    education details added

--- src/components/Education/Education.jsx
+++ src/components/Education/Education.jsx
@@ -8,12 +8,12 @@ function Education() {
                 <div className="educationCard">
                     <h2>Higher Education</h2>
                     <p>- Jeevan Bharti Vidhyalaya</p>
-                    <p>10th - <span>( 73.00% )</span> 12th - <span>( 77.00% )</span></p>
+                    <p>10th - <span> 73.00% </span> 12th - <span> 77.00% </span></p>
                 </div>
                 <div className="educationCard">
                     <h2>Bachelor Education</h2>
                     <p>- S.D Jain International College</p>
-                    <p>( Last Semester Ongoing )</p>
+                    <p>CGPA - 6.37</p>
                 </div>
             </div>
         </>
